10465ceda92f65deab5946892b5e7389f165268c
Zefiruu <53922624+Zefiruu@users.noreply.github.com>
2020-09-08 02:44:25 +0100

    Add delay, start, stop and dot counter functionalities

--- draw.js
+++ draw.js
@@ -1,69 +1,93 @@
-
-    let canvas = document.querySelector(".canvas"),
-        context = canvas.getContext("2d"),
-        canvasPos = canvas.getBoundingClientRect(),
-        clicks = 4000;
-        dotCount = 1,
-        dots = {
-            1: {
-                x: 0,
-                y: 0
-            },
-            2: {
-                x: 0,
-                y: 0
-            },
-            3: {
-                x: 0,
-                y: 0
-            },
-            4: {
-                x: 0,
-                y: 0
-//             },
-            // 5: {
-            //     x: 0,
-            //     y: 0
-            // },
-            // 6: {
-            //     x: 0,
-            //     y: 0
-            }
-        };
-        
-        let  lastDot = { x: 250,
-                         y:  250};
-        
-        alert("Click anywhere to start")
-        function draw(event){
-            let dotX = event.clientX - canvasPos.left,
-            dotY = event.clientY - canvasPos.top;
-            
-            
-                if(dotCount <= 4) {
-                    alert("Selected position " + dotCount);
-                    context.fillStyle = "red";
-                    context.fillRect(dotX, dotY, 5, 5);
-                    dots[dotCount].x = dotX;
-                    dots[dotCount].y = dotY;
-                    dotCount++;
-                    console.log(dotCount);
-                } 
-
-                if(dotCount > 4){
-                    for(let i = 0; i < clicks; i++) {
-                        half(Math.floor(Math.random()*4)+1);            
-                        function half(selectedDot) {
-                            let valX = (dots[selectedDot].x - lastDot.x)/2,
-                                valY = (dots[selectedDot].y - lastDot.y)/2;
-                            context.fillStyle = "#fff";
-                            context.fillRect(lastDot.x, lastDot.y, 1,1);
-                            context.fillRect(lastDot.x + valX, lastDot.y + valY, 1, 1);
-                            lastDot.x += valX;
-                            lastDot.y += valY;
-                    }
-                }
-            }
+let canvas = document.querySelector(".canvas"),
+    context = canvas.getContext("2d"),
+    canvasPos = canvas.getBoundingClientRect(),
+    clicks = 20000,
+    dotCount = 1,
+    dotQty = 3,
+    dots = {
+        1: {
+            x: 0,
+            y: 0
+        },
+        2: {
+            x: 0,
+            y: 0
+        },
+        3: {
+            x: 0,
+            y: 0
+        },
+        4: {
+            x: 0,
+            y: 0
+        },
+        5: {
+            x: 0,
+            y: 0
+        },
+        6: {
+            x: 0,
+            y: 0
         }
-        
-        
+    };
+
+let lastDot = {
+    x: 250,
+    y: 250
+};
+
+let i = 0, j = 0;
+alert('Select the position of the dots, then click "Start"')
+
+function start() {
+    i = j;
+    dotGeneration();
+}
+
+function stop() {
+    j = i;
+    i = clicks;
+}
+
+function dotGeneration() {
+    setTimeout(function () {
+        i++;
+        if(i != 20001) {
+            document.getElementById('dotCount').innerHTML = i;
+        }
+        half(Math.floor(Math.random() * dotQty) + 1);
+        if (i < clicks) {
+            dotGeneration()
+        }
+    }, document.getElementById('speedSlider').value)
+}
+
+function slider() {
+    stop();
+    start();
+}
+
+function half(selectedDot) {
+    let valX = (dots[selectedDot].x - lastDot.x) / 2,
+        valY = (dots[selectedDot].y - lastDot.y) / 2;
+    context.fillStyle = "#fff";
+    context.fillRect(lastDot.x, lastDot.y, 1, 1);
+    context.fillRect(lastDot.x + valX, lastDot.y + valY, 1, 1);
+    lastDot.x += valX;
+    lastDot.y += valY;
+}
+
+function draw(event) {
+    let dotX = event.clientX - canvasPos.left,
+        dotY = event.clientY - canvasPos.top;
+
+    if (dotCount <= dotQty) {
+        alert("Selected position " + dotCount);
+        context.fillStyle = "red";
+        context.fillRect(dotX, dotY, 5, 5);
+        dots[dotCount].x = dotX;
+        dots[dotCount].y = dotY;
+        dotCount++;
+        console.log(dotCount);
+    }
+}
